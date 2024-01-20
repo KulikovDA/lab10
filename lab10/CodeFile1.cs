c72c2a92af59416b228bbdae977d04dd1fe1ce30
Arion <Arion@DESKTOP-9BU80RS>
2024-01-20 03:19:36 +0200

    Добавьте файлы проекта.

---
+++ lab10/CodeFile1.cs
@@ -0,0 +1,7 @@
+﻿MyClass myObject = new MyClass();
+Console.WriteLine(myObject.CalculateAverage());
+Console.WriteLine(myObject.FindMaxNumber());
+Console.WriteLine(myObject.GetInfoString());
+MySubclass mySubObject = new MySubclass();
+Console.WriteLine(mySubObject.CalculateSumOfSquares());
+Console.WriteLine(mySubObject.GetInfoString());
---
+++ lab10/Program.cs
@@ -0,0 +1,75 @@
+﻿public class MyClass
+{
+    public int a;
+    public int b;
+    public int c;
+    public int d;
+    public MyClass()
+    {
+        Console.WriteLine("Введите значение для a:");
+        a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для b:");
+        b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для c:");
+        c = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для d:");
+        d = int.Parse(Console.ReadLine());
+    }
+    public MyClass(int aValue, int bValue, int cValue, int dValue)
+    {
+        a = aValue;
+        b = bValue;
+        c = cValue;
+        d = dValue;
+    }
+    public double CalculateAverage()
+    {
+        return (a + b + c + d) / 4.0;
+    }
+    public int FindMaxNumber()
+    {
+        return Math.Max(Math.Max(a, b), Math.Max(c, d));
+    }
+
+    public string GetInfoString()
+    {
+        return $"a: {a}, b: {b}, c: {c}, d: {d}";
+    }
+}
+class MySubclass : MyClass
+{
+    private int e;
+    private int x;
+    public MySubclass()
+    {
+        Console.WriteLine("Введите значение для e:");
+        e = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для x:");
+        x = int.Parse(Console.ReadLine());
+    }
+    public MySubclass(int aValue, int bValue, int cValue, int dValue, int eValue, int xValue)
+    : base(aValue, bValue, cValue, dValue)
+    {
+        e = eValue;
+        x = xValue;
+    }
+    public int CalculateSumOfSquares()
+    {
+        int kv1 = a - x;
+        int kv2 = b - x;
+        int kv3 = c - x;
+        int kv4 = d - x;
+
+        int sumOfSquares = kv1 * kv1 + kv2 * kv2 + kv3 * kv3 + kv4 * kv4;
+
+        return sumOfSquares;
+    }
+    public new string GetInfoString()
+    {
+        return base.GetInfoString() + $", e: {e}, x: {x}";
+    }
+}
